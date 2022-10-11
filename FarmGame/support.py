adbdfec1079bddbc5e2ef73c4e61a34d26301751
Sebamalicz <93799572+Sebamalicz@users.noreply.github.com>
2022-10-11 09:56:03 -0600

    Delete FarmGame directory

--- FarmGame/support.py
+++
@@ -1,9 +0,0 @@
-from os import walk 
-
-def import_folder(path):
-    surface_list = []
-
-    for folder_name, sub_folder, img_files in walk(path):
-        print(folder)
-
-    return surface_list
